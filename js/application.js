d49d8fc0666b298ce3b363e378b3eda0d735ef6f
Uma Chandran <umachandran6@gmail.com>
2015-08-28 22:58:02 -0400

    Add updated application.js file with input validation, feature that lists previous guesses

--- js/application.js
+++ js/application.js
@@ -9,14 +9,18 @@ $( document ).ready(function() {
  var currentGuess;
  var guesses = [];
  var guessDifference;
+ var differences = [];
  var hint ="";
  var guessesLeft=5;
+ var previousGuesses="";
+ var tempHint="";
 
  console.log(number);
 
  var updateHelpBlock = function (){
     $('.help-block').html(hint);
     hint="";
+    tempHint="";
  }
 
 
@@ -24,49 +28,87 @@ $( document ).ready(function() {
         event.preventDefault();
         if (guessesLeft>0){
           currentGuess=+$('#userGuess').val();
-          guesses.push(currentGuess);
-
-          if (number == currentGuess){
-            console.log("SUCCESS!!!!!!");
-            hint="SUCCESS!!!!!!";
-            updateHelpBlock();
+          if (currentGuess<1 || currentGuess>100 || (currentGuess%1!=0)){
+            alert("Please enter a valid number between 1 and 100");
           }
           else {
+            if (guesses.length>0 && (guesses.indexOf(currentGuess)!=-1)){
+              hint = "You already tried that number. Try again."
+              hint += "<br> Your guesses so far: " + previousGuesses.slice(0,previousGuesses.length-2);
+              updateHelpBlock();
+            } else {
 
-            guessDifference = Math.abs(currentGuess - number);
-
-            if (guessDifference <=10){
-              hint+="You are hot";
-            }
-            else{
-              hint+="You are cold";
-            }
-
-            if (currentGuess<number){
-              hint +=". Guess higher.";
-            }
-            else{
-              hint +=". Guess lower.";
+            if (guesses.length>0){
+              previousGuesses+= guesses[guesses.length-1] +", ";
             }
 
-          guessesLeft--;
-          if (guessesLeft==1){
-            hint += "<br> You have 1 guess left.";
-          } else if (guessesLeft>1){
-            hint += "<br> You have " + guessesLeft + " guesses left.";
-          } else {
-            hint = "Sorry, that's not correct and you're out of guesses. <br>Please hit Replay to play again."
-          }
+            guesses.push(currentGuess);
 
-          console.log (hint + "You have " + guessesLeft + " guesse(s) left.");
-          updateHelpBlock();
+            if (number == currentGuess){
+              console.log("SUCCESS!!!!!!");
+              hint="SUCCESS!!!!!!<br>Hit Replay to play again.";
+              updateHelpBlock();
+              $('.guess').prop("disabled", true);
+            }
+            else {
 
+              guessDifference = Math.abs(currentGuess - number);
+              differences.push(guessDifference);
+
+              if (differences.length>1){
+
+                 if (guessDifference<=differences[differences.length-2]){
+                    tempHint+="You are getting hotter";
+                 } else {
+                    tempHint+="You are getting colder";
+                 }
+
+              }
+
+              if (guessDifference <=10){
+                hint+="You are hot. "; 
+                if (differences.length>1){
+                  hint+= tempHint;
+                }
+
+              }
+              else{
+                hint+="You are cold. ";
+                if (differences.length>1){
+                  hint+= tempHint;
+                }
+              }
+
+              if (currentGuess<number){
+                hint +=" Guess higher.";
+              }
+              else{
+                hint +=" Guess lower.";
+              }
+
+              guessesLeft--;
+              if (guessesLeft==1){
+                hint += "<br> You have 1 guess left.";
+              } else if (guessesLeft>1){
+                hint += "<br> You have " + guessesLeft + " guesses left.";
+              } else {
+                hint = "Sorry, that's not correct and you're out of guesses. <br> The correct answer was " + number + ". Please hit Replay to play again."
+                $('.guess').prop("disabled", true);
+              }
+
+              if (previousGuesses!="" && !($('.guess').prop("disabled"))) {
+                  hint += "<br> Your guesses so far: " + previousGuesses.slice(0,previousGuesses.length-2);
+              }
+
+              console.log (hint + "You have " + guessesLeft + " guesse(s) left.");
+              updateHelpBlock();
+              }
+            }
           }
         } else {
-          hint += "Sorry, you're out of guesses. <br>Please hit Replay to play again."
-          updateHelpBlock();
-        }
-            
+            hint += "Sorry, you're out of guesses. <br>Please hit Replay to play again."
+            updateHelpBlock();
+      }
  });
 
  $('.hint').on('click', function(){
@@ -79,9 +121,12 @@ $( document ).ready(function() {
  
  $('.replay').on('click', function(){
     event.preventDefault();
+    $('.guess').prop("disabled", false);
     number = Math.floor((Math.random() * 100) + 1);
     guesses = [];
+    differences = [];
     guessesLeft=5;
+    previousGuess="";
     $('#userGuess').val("1-100");
     console.log("new number " + number);
     hint = "Enter a number between 1 and 100. <br> You have 5 guesses left.";
